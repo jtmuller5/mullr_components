c5908f2d10e60bd6109c32b7b9ea5d4d3cecc455
jtmuller5 <47997351+jtmuller5@users.noreply.github.com>
2021-12-05 09:05:19 -0500

    Updated mason

--- mason/bricks/main/__brick__/lib/services/firebase/cloud_messaging_service.dart
+++ mason/bricks/main/__brick__/lib/services/firebase/cloud_messaging_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import '../../services.dart';
+import '../services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
