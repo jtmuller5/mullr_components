225597b94fb771af434f2466910426472070ec8d
jtmuller5 <jtmuller5@gmail.com>
2022-01-17 19:36:10 -0500

    u

--- lib/services/system_service.dart
+++ lib/services/system_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
