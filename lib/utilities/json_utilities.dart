2f39dca41110459610b3e6a91792c2f2f4dc3a79
jtmuller5 <jtmuller5@gmail.com>
2022-08-07 08:06:33 -0400

    u

--- lib/utilities/json_utilities.dart
+++ lib/utilities/json_utilities.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get_it/get_it.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
 
 DateTime? getDateFromTimestamp(dynamic timestamp) {
